6e3a89178cbba19205d5c3aeea834e2884f990e8
Charli Russell <charlirussell2491@gmail.com>
2025-01-18 16:40:46 +0100

    - Added Read More button to paragraph for better visual appeal, especially on mobile devices. - Updated state logic by separating isOpen into isReadMoreOpen and isLightboxOpen to handle text expansion and lightbox functionality independently. - Added custom SCSS styles to the Read More button to prevent global button styles from being applied.

--- scarredsteel/src/components/Home/Home.jsx
+++ scarredsteel/src/components/Home/Home.jsx
@@ -8,8 +8,8 @@ import './Home.scss';
 
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [isReadMoreOpen, setIsReadMoreOpen] = useState(false);
   const [images, setImages] = useState ([]);
 
   useEffect(() => {
@@ -30,6 +30,10 @@ const Home = () => {
     importImages();
   }, []);
 
+  const toggleText = () => {
+    setIsReadMoreOpen(!isReadMoreOpen);
+  };
+
   return (
     <>
       <div className='home-main'>
@@ -46,19 +50,30 @@ const Home = () => {
       <div className='home-section'>
         <div className='home-paragraph'>
           <h1>Our Services</h1>
-          <p>At Scarred Steel, we specialize in high-quality metal restoration and fabrication services in Guernsey. Whether you need fire escapes, gates, or other metalwork projects restored or custom-built, our expert team has the skills and experience to deliver exceptional results. From welding and fabrication to comprehensive restoration, we pride ourselves on outstanding craftsmanship and customer service. No project is too big or small, and we ensure every detail is perfect. If you're looking for reliable metal restoration and fabrication services in Guernsey, contact us today to discuss how we can bring your metalwork back to life.</p>
+          <p>
+            At Scarred Steel, we specialize in high-quality metal restoration and fabrication services in Guernsey. Whether you need fire escapes, gates, or other metalwork projects restored or custom-built, our expert team has the skills and experience to deliver exceptional results.
+            {isReadMoreOpen && (
+              <>
+              {' '}
+              From welding and fabrication to comprehensive restoration, we pride ourselves on outstanding craftsmanship and customer service. No project is too big or small, and we ensure every detail is perfect. If you're looking for reliable metal restoration and fabrication services in Guernsey, contact us today to discuss how we can bring your metalwork back to life.
+              </>
+            )}
+          </p>
+          <button onClick={toggleText} className='read-more-btn'>
+            {isReadMoreOpen ? 'Read Less' : 'Read More'}
+          </button>
         </div>
         <div className='home-gallery'>
           {/* Lightbox Thumbnail */}
           <div classname='lightbox-container'>
-            <button className='thumbnail-btn' type='button' onClick={() => setIsOpen(true)}>
+            <button className='thumbnail-btn' type='button' onClick={() => setIsLightboxOpen(true)}>
               <img src={ HomeGalleryTn } alt="Thumbnail1" class="thumbnail" />
             </button>
           </div>
           {/* Lightbox */}
           <Lightbox
-            open={isOpen}
-            close={() => setIsOpen(false)}
+            open={isLightboxOpen}
+            close={() => setIsLightboxOpen(false)}
             slides={images}
           />
         </div>
