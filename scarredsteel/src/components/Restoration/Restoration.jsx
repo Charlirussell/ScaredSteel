3c9517680f348ef403065b8e71dfdca7b2bd5efe
Charli Russell <charlirussell2491@gmail.com>
2025-01-18 22:02:15 +0100

    - Integrated and set up Lightbox on the restoration page, as on the other pages. -Added flex to restoration section to center the lighbox thumbnail.

--- scarredsteel/src/components/Restoration/Restoration.jsx
+++ scarredsteel/src/components/Restoration/Restoration.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 import RestorationImg from "/src/assets/images/gate.jpeg";
 import RestoGalleryTn from "/src/assets/images/resto-gallery-tn.webp";
+
 import './Restoration.scss';
 
 const Restoration = () => {
+    const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+    const [images, setImages] = useState ([]);
   
+    useEffect(() => {
+      const importImages = async () => {
+        const imagePaths = [
+          import('../../assets/images/wheels-1.jpg'),
+          import('../../assets/images/wheels-2.jpg'),
+          import('../../assets/images/wheels-3.jpg'),
+          import('../../assets/images/wheels-4.jpg'),
+          import('../../assets/images/wheels-5.jpg'),
+          import('../../assets/images/wheels-6.jpg'),
+          import('../../assets/images/wheels.jpg'),
+        ];
+        const loadedImages = await Promise.all(imagePaths);
+  
+        setImages(loadedImages.map(image => ({ src: image.default })));
+      };
+      importImages();
+    }, []);
+
   return (
     <>
       <div className='restoration-main'>
@@ -23,9 +46,20 @@ const Restoration = () => {
             <h1>Restoration Services</h1>
             <p>At Scarred Steel, we specialize in the restoration and repair of various metalwork projects in Guernsey, including fire escapes, gates, and more. From intricate wrought iron details to essential repairs, we focus on delivering exceptional craftsmanship and top-tier service. Our experienced team uses advanced restoration techniques and high-quality materials to bring aged or damaged metalwork back to life. Whether you need restoration for outdoor structures, industrial machinery, or custom gates, we're here to help. Contact us today to learn how Scarred Steel can transform your metalwork with our expert restoration services.</p>
         </div>
-          <div className='restoration-gallery'>
-            <img src={ RestoGalleryTn } alt="" />
-          </div> 
+        <div className='restoration-gallery'>
+            {/* Lightbox Thumbnail */}
+          <div classname='lightbox-container'>
+            <button className='thumbnail-btn' type='button' onClick={() => setIsLightboxOpen(true)}>
+              <img src={ RestoGalleryTn } alt="Thumbnail1" class="thumbnail" />
+            </button>
+          </div>
+          {/* Lightbox */}
+          <Lightbox
+            open={isLightboxOpen}
+            close={() => setIsLightboxOpen(false)}
+            slides={images}
+          /> 
+        </div>
       </div>
     </>
   );
